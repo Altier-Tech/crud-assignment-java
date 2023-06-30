c07e0dd455fb0bc2ae876d5b1e3255f22cfacb13
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-30 22:59:10 +0530

    Update Connector.java

--- dba/src/main/java/tech/altier/dba/Connector.java
+++ dba/src/main/java/tech/altier/dba/Connector.java
@@ -19,6 +19,38 @@ public class Connector {
             e.printStackTrace();
         }
     }
+    
+    public static Item getItemByID(int id) {
+        String SQL = "SELECT * FROM `inventory`.`items` WHERE `id` = " + id + ";";
+
+        try {
+            var result = _connection.createStatement().executeQuery(SQL);
+            if (result.next()) {
+                return new Item(result.getInt("id"), result.getString("name"), result.getString("description"), result.getInt("quantity"), result.getDouble("price"));
+            } else {
+                return null;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static boolean addItem(Item item) {
+        return add(item.getName(), item.getDescription(), item.getQuantity(), item.getPrice());
+    }
+
+    public static boolean add(String name, String description, int quantity, double price) {
+        String SQL = "INSERT INTO `inventory`.`items` (`name`, `description`, `quantity`, `price`) VALUES ('" + name + "', '" + description + "', '" + quantity + "', '" + price + "');";
+
+        try {
+            _connection.createStatement().execute(SQL);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
 
     private static boolean createTable() {
         String SQL =    "CREATE TABLE IF NOT EXISTS `inventory`.`items` (\n" +
