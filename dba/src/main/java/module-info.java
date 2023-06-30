54fbd41109c7b84ba384ac18d67e1b5ca37a4300
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-30 22:49:36 +0530

    Update module-info.java

--- dba/src/main/java/module-info.java
+++ dba/src/main/java/module-info.java
@@ -3,6 +3,8 @@ module tech.altier.crudassignment {
     requires javafx.fxml;
 
     requires org.kordamp.bootstrapfx.core;
+    requires mysql.connector.j;
+    requires java.sql;
 
     opens tech.altier.crudassignment to javafx.fxml;
     exports tech.altier.crudassignment;
