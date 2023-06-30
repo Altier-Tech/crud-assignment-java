bddab8fd74a96a91250ae3993adb3cc6dd72c1d0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-07-01 02:12:36 +0530

    Update MainApplication.java

--- dba/src/main/java/tech/altier/crudassignment/MainApplication.java
+++ dba/src/main/java/tech/altier/crudassignment/MainApplication.java
@@ -8,13 +8,20 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApplication extends Application {
+    static Stage stage;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 720, 480);
         stage.setTitle("Inventory Management System");
         stage.setScene(scene);
+        MainApplication.stage = stage;
         stage.show();
+    }
+
+    public static Stage getStage() {
+        return stage;
     }
 
     public static void main(String[] args) {
