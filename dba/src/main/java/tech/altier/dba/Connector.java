e41fd3fd82e466695067556da70d962a63381966
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-30 22:50:33 +0530

    Update Connector.java

--- dba/src/main/java/tech/altier/dba/Connector.java
+++ dba/src/main/java/tech/altier/dba/Connector.java
@@ -5,12 +5,19 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connector {
-    // connect to localhost mysql server
-    // with username root and password blank
-    // and database name inventory
     private static final String _DB_URL = "jdbc:mysql://localhost:3306/inventory";
     private static final String _USERNAME = "root";
     private static final String _PASSWORD = "";
+    
+    private static Connection _connection;
+
+    static {
+        try {
+            _connection = connect();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static Connection connect() throws SQLException {
         return DriverManager.getConnection(_DB_URL, _USERNAME, _PASSWORD);
