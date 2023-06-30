9df7450cb97c8044cada52b9bbba7acdbd02aa34
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-07-01 03:16:29 +0530

    Update Item.java

--- dba/src/main/java/tech/altier/dba/Item.java
+++ dba/src/main/java/tech/altier/dba/Item.java
@@ -2,27 +2,25 @@ package tech.altier.dba;
 
 public class Item {
 
-    private final int id;
-    private final String name;
-    private final String description;
-    private final int quantity;
-    private final double price;
+    public int id;
+    public String name;
+    public String description;
+    public int quantity;
+    public double price;
+
+    public Item(String name, String description, int quantity, double price) {
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
 
     public Item() {
-        this.id = 0;
+        this.id = -1;
         this.name = "";
         this.description = "";
         this.quantity = 0;
         this.price = 0.0;
-    }
-
-    public Item(String sqlReturnString) {
-        String[] split = sqlReturnString.split(",");
-        this.id = Integer.parseInt(split[0]);
-        this.name = split[1];
-        this.description = split[2];
-        this.quantity = Integer.parseInt(split[3]);
-        this.price = Double.parseDouble(split[4]);
     }
 
     public Item(int id, String name, String description, int quantity, double price) {
@@ -33,56 +31,40 @@ public class Item {
         this.price = price;
     }
 
-    public Item(String name, String description, int quantity, double price) {
-        this.id = 0;
-        this.name = name;
-        this.description = description;
-        this.quantity = quantity;
-        this.price = price;
-    }
-
     public int getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public int getQuantity() {
-        return this.quantity;
-    }
-
-    public double getPrice() {
-        return this.price;
+        return id;
     }
 
     public void setId(int id) {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public void setName(String name) {
-        if (name.length() > 45) {
-            this.name = name.substring(0, 45);
-        } else {
-            this.name = name;
-        }
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public void setDescription(String description) {
-        if (description.length() > 45) {
-            this.description = description.substring(0, 45);
-        } else {
-            this.description = description;
-        }
+        this.description = description;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
     }
 
     public void setPrice(double price) {
