e92859222142d6ea589462bb47e91edd7fbd9d2e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-06-30 22:52:24 +0530

    Update Connector.java

--- dba/src/main/java/tech/altier/dba/Connector.java
+++ dba/src/main/java/tech/altier/dba/Connector.java
@@ -8,7 +8,7 @@ public class Connector {
     private static final String _DB_URL = "jdbc:mysql://localhost:3306/inventory";
     private static final String _USERNAME = "root";
     private static final String _PASSWORD = "";
-    
+
     private static Connection _connection;
 
     static {
@@ -16,6 +16,24 @@ public class Connector {
             _connection = connect();
         } catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static boolean createTable() {
+        String SQL =    "CREATE TABLE IF NOT EXISTS `inventory`.`items` (\n" +
+                        "  `id` INT NOT NULL AUTO_INCREMENT,\n" +
+                        "  `name` VARCHAR(45) NOT NULL,\n" +
+                        "  `description` VARCHAR(45) NOT NULL,\n" +
+                        "  `quantity` INT NOT NULL,\n" +
+                        "  `price` DOUBLE NOT NULL,\n" +
+                        "  PRIMARY KEY (`id`));";
+        
+        try {
+            _connection.createStatement().execute(SQL);
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
         }
     }
 
